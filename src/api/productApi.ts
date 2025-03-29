ae7e3699f2df906d3816c9afaac3217b38c1d0e6
4th-KiNG <95131486+4th-KiNG@users.noreply.github.com>
2025-03-29 22:48:58 +0300

    add send data

--- src/api/productApi.ts
+++ src/api/productApi.ts
@@ -1,3 +1,6 @@
 import { API, Http } from "../constants/api";
 
 export const GetProducts = async () => Http.get(`${API}/products`);
+
+export const SendData = async (userId: string) =>
+  Http.post(`${API}/products/order`, { userId: userId });
--- src/pages/CheckoutPage/CheckoutPage.tsx
+++ src/pages/CheckoutPage/CheckoutPage.tsx
@@ -2,23 +2,27 @@ import { observer } from "mobx-react-lite";
 import checkout from "../../storage/checkout";
 import { CheckoutProduct } from "../../components";
 import { tgApp } from "../../constants/api";
+import { SendData } from "../../api/productApi";
 
 const CheckoutPage = observer(() => {
   const { getProducts } = checkout;
-  tgApp.MainButton.setText("Оформить заказ").show();
 
-  tgApp.MainButton.onClick(() => {
-    const data = JSON.stringify({
-      productId: getProducts[0].id,
-    });
-    tgApp.sendData(data);
-  });
-
+  const handleSendData = () => {
+    SendData(tgApp.initDataUnsafe.user?.id.toString() ?? "").then((data) =>
+      console.log(data)
+    );
+  };
   return (
     <div className="relative grid grid-cols-[1fr_1fr] gap-2 p-4 pb-12 h-full">
       {getProducts.map((product) => (
         <CheckoutProduct {...product} key={product.id} />
       ))}
+      <button
+        className="fixed bottom-2 left-2 w-[calc(100%-16px)] py-2 bg-white box-border text-black rounded-md"
+        onClick={handleSendData}
+      >
+        Оформить заказ
+      </button>
     </div>
   );
 });
