32b7bfeb038c5a1906dc59c1bc735388ff3d6228
4th-KiNG <95131486+4th-KiNG@users.noreply.github.com>
2025-03-29 22:01:01 +0300

    add main button

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -6,3 +6,6 @@ export const API = "http://localhost:3000";
 export const Http = axios.create();
 
 export const tgApp = WebApp;
+
+tgApp.expand();
+tgApp.MainButton.setText("Открыть каталог").show();
