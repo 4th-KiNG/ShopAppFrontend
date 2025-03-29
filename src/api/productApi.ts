a9de304b73f36856177acf8686417e50292a6121
4th-KiNG <95131486+4th-KiNG@users.noreply.github.com>
2025-03-29 21:37:54 +0300

    add chat id

--- src/api/productApi.ts
+++ src/api/productApi.ts
@@ -4,6 +4,7 @@ export const GetProducts = async () => Http.get(`${API}/products`);
 
 export const SendOfferData = async (productId: string) => {
   const chatId = tgApp.initDataUnsafe.chat?.id;
+  console.log(chatId);
   if (chatId) {
     await Http.post(`https://api.telegram.org/bot${BOT_TOKEN}/sendMessage`, {
       chatId: chatId,
