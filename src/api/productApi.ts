299af6d132afdb6ae3fbf79ce86317ea52eb0d31
4th-KiNG <95131486+4th-KiNG@users.noreply.github.com>
2025-03-29 22:56:13 +0300

    12345

--- src/api/productApi.ts
+++ src/api/productApi.ts
@@ -2,5 +2,5 @@ import { API, Http } from "../constants/api";
 
 export const GetProducts = async () => Http.get(`${API}/products`);
 
-export const SendData = async (userId: string) =>
-  Http.post(`${API}/products/order`, { userId: userId });
+export const SendData = async (username: string) =>
+  Http.post(`${API}/products/order`, { username: username });
