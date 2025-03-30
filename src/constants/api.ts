6c1cc3f22a2b55680adbdd8ce0b922f8bae8908c
4th-KiNG <95131486+4th-KiNG@users.noreply.github.com>
2025-03-30 17:05:52 +0300

    change api

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 import WebApp from "@twa-dev/sdk";
 
-export const API = "http://localhost:3000";
+//export const API = "http://localhost:3000";
+export const API = "http://185.221.198.195:3000";
 export const STORAGE = `${API}/storage`;
 
 export const Http = axios.create();
